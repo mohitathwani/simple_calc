608d20ccbe8f93bed5a6ec9e18cdb66c4ca84fc1
Mohit Athwani <mohit@athwani.net>
2020-04-11 09:54:37 -0700

    introduced multiply

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 
+float multiply(float a, float b)
+{
+    return a * b;
+}
 float add(float a, float b)
 {
-    return a + b;
+    return a / b;
 }
 
 int main()
 {
     std::cout << add(5,3) << std::endl;
+    std::cout << multiply(2,4) << std::endl;
     return 0;
 }
